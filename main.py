393a7149b2a53a148446877d7693da2c262c9e80
ste <belli29@hotmail.com>
2021-05-28 23:26:19 +0200

    added function

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import json
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def read_and_write(my_dict):
+    j = json.dumps(my_dict)
+    with open('a.json', 'w') as f:
+        f.write(j)
 
 
-# Press the green button in the gutter to run the script.
+my_dicto = {
+    'name': "shila",
+    'animal': "dog",
+    'age': 13
+}
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    read_and_write(my_dicto)
